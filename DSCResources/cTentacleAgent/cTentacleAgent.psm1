558b12d6d03647e2920c89f5d98d5e2573fa2275
Nico Engelen <engelen.nico@googlemail.com>
2014-12-10 14:11:03 +0000

    Replacing another Write-Host with Write-Verbose

--- DSCResources/cTentacleAgent/cTentacleAgent.psm1
+++ DSCResources/cTentacleAgent/cTentacleAgent.psm1
@@ -214,7 +214,7 @@ function Request-File
         [string]$saveAs
     )
  
-    Write-Host "Downloading $url to $saveAs"
+    Write-Verbose "Downloading $url to $saveAs"
     $downloader = new-object System.Net.WebClient
     $downloader.DownloadFile($url, $saveAs)
 }
